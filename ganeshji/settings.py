64c0dc3c2d82d6cedeef7942ab191c1327655842
sanabinadaf <71943182+ishannadaf@users.noreply.github.com>
2026-02-07 21:53:12 +0530

    Deployment changes

--- ganeshji/settings.py
+++ ganeshji/settings.py
@@ -122,13 +122,14 @@ WSGI_APPLICATION = "ganeshji.wsgi.application"
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
+        'NAME': os.getenv('MYSQLDATABASE'),
+        'USER': os.getenv('MYSQLUSER'),
+        'PASSWORD': os.getenv('MYSQLPASSWORD'),
+        'HOST': os.getenv('MYSQLHOST'),
+        'PORT': os.getenv('MYSQLPORT'),
         "OPTIONS": {
             "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
+            "charset": "utf8mb4",
         },
         "CONN_MAX_AGE": 100,
     }
